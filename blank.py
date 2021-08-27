595b08c8c3ad78b70c60ffcbfb7bee4ffb31e56b
shadowquark <shadowquark@outlook.com>
2021-08-28 01:08:34 +0200

    newblank

--- blank.py
+++ blank.py
@@ -13,16 +13,12 @@ import random as rnd
 # g(f([x1, x2...])) -> FF([x1, x2...], f, g...)
 F = lambda z: [*ft.reduce(lambda x, y: map(y, x), [z[:1]] + z[1:])][0]
 FF = lambda *z: [*ft.reduce(lambda x, y: map(y, x), z)]
-# f((x1, x2...)) -> ff1(f)(x1, x2...)
-# f(x1, x2..., y1, y2...) -> ff2(f, (y1, y2...))((x1, x2...))
-# f(x1, x2..., y1, y2...) -> ff3(f, (x1, x2...))((y1, y2...))
-# f(x1, x2..., y1, y2...) -> fff(f, y1, y2...)(x1, x2...)
-# f(x1, x2..., y1, y2...) -> FFF(f, x1, x2...)(y1, y2...)
-ff1 = lambda f: lambda *x: f(x)
-ff2 = lambda f, x: lambda y: f(*y, *x)
-ff3 = lambda f, x: lambda y: f(*x, *y)
-fff = lambda f, *x: ff1(ff2(f, x))
-FFF = lambda f, *x: ff1(ff3(f, x))
+# f(x1, x2..., y1, y2...) -> fxy(f, x1, x2...)(y1, y2...)
+# f(x1, x2..., y1, y2...) -> fyx(f, y1, y2...)(x1, x2...)
+# f(x1, x2..., y1 = y1, y2 = y2...) -> fYx(f, y1 = y1, y2 = y2...)(x1, x2...)
+fxy = lambda f, *x: lambda *y: f(*x, *y)
+fyx = lambda f, *x: lambda *y: f(*y, *x)
+fYx = lambda f, **x: lambda *y: f(*y, **x)
 
 fin = open(".in", 'r')
 fout = open(".out", 'w')
