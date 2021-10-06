efc1c563ccd43874e49ee308937ffce8d757fc61
shadowquark <shadowquark@outlook.com>
2021-10-06 04:18:25 +0200

    newF

--- blank.py
+++ blank.py
@@ -11,7 +11,9 @@ import random as rnd
 
 # g(f(x)) -> F([x, f, g...])
 # g(f([x1, x2...])) -> FF([x1, x2...], f, g...)
-F = lambda z: [*ft.reduce(lambda x, y: map(y, x), [z[:1]] + z[1:])][0]
+def F(*z):
+    z = list(z)
+    return [*ft.reduce(lambda x, y: map(y, x), [z[:1]] + z[1:])][0]
 FF = lambda *z: [*ft.reduce(lambda x, y: map(y, x), z)]
 # f(x1, x2..., y1, y2...) -> fxy(f, x1, x2...)(y1, y2...)
 # f(x1, x2..., y1, y2...) -> fyx(f, y1, y2...)(x1, x2...)
@@ -22,8 +24,6 @@ fYx = lambda f, **x: lambda *y: f(*y, **x)
 
 fin = open(".in", 'r')
 fout = open(".out", 'w')
-
-
 
 fout.close()
 
