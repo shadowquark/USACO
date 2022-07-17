e3168d4782ef9d991ab9f34a41d8985fd8c47b1d
shadowquark <shadowquark@outlook.com>
2022-07-17 03:41:52 +0200

    blank

--- blank.py
+++ blank.py
@@ -15,12 +15,8 @@ def F(*z):
     z = list(z)
     return [*ft.reduce(lambda x, y: map(y, x), [z[:1]] + z[1:])][0]
 FF = lambda *z: [*ft.reduce(lambda x, y: map(y, x), z)]
-# f(x1, x2..., y1, y2...) -> fxy(f, x1, x2...)(y1, y2...)
 # f(x1, x2..., y1, y2...) -> fyx(f, y1, y2...)(x1, x2...)
-# f(x1, x2..., y1 = y1, y2 = y2...) -> fYx(f, y1 = y1, y2 = y2...)(x1, x2...)
-fxy = lambda f, *x: lambda *y: f(*x, *y)
 fyx = lambda f, *x: lambda *y: f(*y, *x)
-fYx = lambda f, **x: lambda *y: f(*y, **x)
 
 fin = open(".in", 'r')
 fout = open(".out", 'w')
