f64d0205bb8161b97e8d276152670e31411b94c6
shadowquark <shadowquark@outlook.com>
2022-10-14 01:52:13 +0200

    dfs

--- 1/4/crypt1.py
+++ 1/4/crypt1.py
@@ -22,36 +22,50 @@ sys.setrecursionlimit(10000)
     x2  x1  x0      x[0]
         x4  x3      x[1]
 ---------------
-    x7  x6  x5      x[2]
-x10 x9  x8          x[3]
+    x7  x6  x5      
+x10 x9  x8          
 ---------------
-x14 x13 x12 x11     x[4]
+x14 x13 x12 x11
 """
-tot = 0
-num = [3, 2, 3, 3, 4]
-b = []
-def construct_x(m, n):
-    if m == n:
-        return 
-for i in num:
-    temp1, temp2, temp3 = 0, 1, []
-    for j in range(i):
-        for k in a:
-            temp1 +=
-        temp2 *= 10
-        temp3.append
-            
-x = []
-def dfs(x):
-    if len(x) == 5:
-        if not ((x[1] % 10 * x[0] - x[2])
-                or (x[1] // 10 * x[0] - x[3])
-                or (x[3] * 10 + x[2] - x[4])):
-            tot += 1
-        return x[:-1]
-    for i in range(n):
-        x = dfs(x)
-    return x[:-1]
-dfs(x)
-print(tot)
+def construct_x(x, out, p, q):
+    if p == q:
+        out.append(x)
+        return out
+    for y in a:
+        if not (p or y):
+            continue
+        x += y * 10 ** (q - p - 1)
+        out = construct_x(x, out, p + 1, q)
+        x -= y * 10 ** (q - p - 1)
+    return out
+nums, b = [3, 2], []
+for num in nums:
+    b.append(construct_x(0, [], 0, num))
+def check(x, version):
+    if version:
+        if (x < 10000 and x > 999 and x // 1000 in a
+            and x // 100 % 10 in a and x // 10 % 10 in a and x % 10 in a):
+            return 1
+        else:
+            return 0
+    else:
+        if (x < 1000 and x > 99
+            and x // 100 in a and x // 10 % 10 in a and x % 10 in a):
+            return 1
+        else:
+            return 0
+def dfs(tot, x):
+    if len(x) == 2:
+        if (check(x[1] % 10 * x[0], 0)
+            and check(x[1] // 10 * x[0], 0)
+            and check(x[0] * x[1], 1)):
+            return tot + 1, x[:-1]
+        else:
+            return tot, x[:-1]
+    for y in b[len(x)]:
+        x.append(y)
+        tot, x = dfs(tot, x)
+    return tot, x[:-1]
+tot, _ = dfs(0, [])
+fout.write(str(tot) + '\n')
 
