03756309a9f43e3bee089c7cf48d37a03058b22b
shadowquark <shadowquark@outlook.com>
2022-11-20 21:49:57 +0100

    blank.py

--- blank.py
+++ blank.py
@@ -13,7 +13,8 @@ import random as rnd
 # g(f([x1, x2...])) -> FF([x1, x2...], f, g...)
 def F(*z):
     z = [*z]
-    return [*reduce(lambda x, y: map(y, x), [z[:1]] + z[1:])][0]
+    z[0] = [z[0]]
+    return [*reduce(lambda x, y: map(y, x), z)][0]
 FF = lambda *z: [*reduce(lambda x, y: map(y, x), z)]
 # f(x1, x2..., y1, y2...) -> fyx(f, y1, y2...)(x1, x2...)
 fyx = lambda f, *x: lambda *y: f(*y, *x)
